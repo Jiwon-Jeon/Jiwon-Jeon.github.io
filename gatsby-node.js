45ecc95aa0642226dc8341270ee92f0aac319d10
Jiwon Jeon <hocldy@naver.com>
2021-04-27 18:02:46 +0900

    fix type error ing

--- gatsby-node.js
+++ gatsby-node.js
@@ -108,7 +108,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       date: Date @dateformat
       tag: String
       writter: String
-      thumbnail: Object
+      thumbnail: [String]!
     }
 
     type Fields {
