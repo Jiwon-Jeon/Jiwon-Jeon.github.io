759dc19b0a93ef92c6577e9f816752a0155b9f40
Jiwon Jeon <hocldy@naver.com>
2021-04-28 16:26:20 +0900

    fix: route error fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,57 +1,58 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import "../assets/index.scss";
-import logo from "../images/logos/linco-logo.png";
-import Search from "./Search";
-import Seo from "./seo";
+import * as React from 'react';
+import {Link} from 'gatsby';
+import '../assets/index.scss';
+import logo from '../images/logos/linco-logo.png';
+import Search from './search';
+import Seo from './seo';
 
-const Layout = ({ location, title, children }) => {
-    const rootPath = `${__PATH_PREFIX__}/`
-    const isRootPath = location.pathname === rootPath
-    let header
+const Layout = ({location, title, children}) => {
+  const rootPath = `${__PATH_PREFIX__}/`;
+  const isRootPath = location.pathname === rootPath;
+  let header;
 
-    if (isRootPath) {
-        header = (
-            <div className='lc-header-wrap'>
-                <h1>
-                    <Link to="/" id='lcLogo'>
-                        <img src={logo} alt="Linco logo"/>
-                        <span>{title}</span>
-                    </Link>
-                </h1>
-                <div className="lc-src-wrap">
-                    <button className='lc-src-btn' />
-                </div>
-            </div>
-        )
-    } else {
-        header = (
-            <div className='lc-header-wrap'>
-                <h1>
-                    <Link to="/" id='lcLogo'>
-                        <img src={logo} alt="Linco logo"/>
-                        <span>{title}</span>
-                    </Link>
-                </h1>
-                <div className="lc-src-wrap">
-                    <button className='lc-src-btn' />
-                </div>
-            </div>
-        )
-    }
-
-    return (
-        <div className="global-wrapper" data-is-root-path={isRootPath}>
-            <header className="global-header">{header}</header>
-            {/*<Search />*/}
-            <main>{children}</main>
-            <footer className='global-footer'>
-                © {new Date().getFullYear()} <a href="https://linco.co.kr">Linco</a>, Built with
-                {` `}
-                <a href="https://www.gatsbyjs.com">Gatsby</a>
-            </footer>
+  if (isRootPath) {
+    header = (
+      <div className="lc-header-wrap">
+        <h1>
+          <Link to="/" id="lcLogo">
+            <img src={logo} alt="Linco logo" />
+            <span>{title}</span>
+          </Link>
+        </h1>
+        <div className="lc-src-wrap">
+          <button className="lc-src-btn" />
         </div>
-    )
-}
+      </div>
+    );
+  } else {
+    header = (
+      <div className="lc-header-wrap">
+        <h1>
+          <Link to="/" id="lcLogo">
+            <img src={logo} alt="Linco logo" />
+            <span>{title}</span>
+          </Link>
+        </h1>
+        <div className="lc-src-wrap">
+          <button className="lc-src-btn" />
+        </div>
+      </div>
+    );
+  }
 
-export default Layout
+  return (
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">{header}</header>
+      {/*<Search />*/}
+      <main>{children}</main>
+      <footer className="global-footer">
+        © {new Date().getFullYear()} <a href="https://linco.co.kr">Linco</a>,
+        Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
